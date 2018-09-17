66b73d005e5c390e0d1a073356c0c2feb7692cd2
Dawid Rusnak <dawid@drcode.pl>
2018-09-17 15:01:23 +0200

    Remove unnecessary logs

--- src/removeRedundantVariables.js
+++ src/removeRedundantVariables.js
@@ -12,11 +12,6 @@ function removeRedundantVariables (content, format) {
   const matches = {}
 
   return content.replace(regex, ($0, name) => {
-    if ($0.indexOf('grid') !== -1) {
-      console.log(name)
-      console.log('-----')
-      console.log($0.length > 200 ? $0.substr(0, 200) + '...' : $0)
-    }
     if (matches[name]) {
       return ''
     }
